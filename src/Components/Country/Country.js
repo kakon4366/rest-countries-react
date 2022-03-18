9402b358fac72278b4e155d0ad8fd5389c0a97f1
kakon4366 <kakonbarman4366@gmail.com>
2022-03-18 17:26:26 +0600

    Showing all country name successfully

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Country = (props) => {
-    console.log(props);
+    console.log(props.country);
+    const {name, flags, timezones, currencies} = props.country;
     return (
         <div>
-            <h2>{}</h2>
+            <h2>{name.common}</h2>
         </div>
     );
 };
